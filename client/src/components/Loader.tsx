53c604e7f87026d0d6c58875d83a43ba2b6a6c0c
Asaiy420 <kcaryan05@gmail.com>
2025-08-01 12:31:29 +0545

    add: implement Loader component with spinner animation

--- client/src/components/Loader.tsx
+++ client/src/components/Loader.tsx
@@ -1,7 +1,11 @@
 
 const Loader = () => {
   return (
-    <div>Loader</div>
+    <div className="flex justify-center items-center py-3">
+      <div 
+        className="animate-spin rounded-full size-12 border-b-2 border-blue-700"
+      />
+    </div>
   )
 }
 
