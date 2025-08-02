006ad0c95b647477ba1ed122c35e9e44af9e254f
Asaiy420 <kcaryan05@gmail.com>
2025-08-02 18:44:38 +0545

    feat: add deployment script and configuration for Transactions contract
    - Created a deployment script for the Transactions contract
    - Updated Hardhat configuration to include Sepolia network settings
    - Added Transactions.json file containing contract ABI and bytecode

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,7 +1,11 @@
 require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-ethers");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/DuG0WNnbinvcFAXZwOScp",
+      accounts: [ '7d5195e916c1007166c36e9ade2fa553f9b1de5f70076b5806a42e0b6099d5b3' ]
+    },
+  },
 };
